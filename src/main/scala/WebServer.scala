a4a4b81f943d88e06d4bd0c2d5f94301fb1ffee4
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-08-08 12:01:01 +0300

    add Envelop

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -3,14 +3,22 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.server.Route
-import spray.json.DefaultJsonProtocol._
-
+import spray.json._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
 
-case class Person(name: String, age: Int)
+trait Message
+final case class Person(name: String, age: Int) extends Message
+final case class Envelope[A <: Message](payload: A, server: String)
+
+object Envelope {
+  def apply[A <: Message](payload: A): Envelope[A] = Envelope(payload, java.net.InetAddress.getLocalHost.toString)
+}
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit def envelopeFormat[A <: Message : JsonFormat] = jsonFormat2(Envelope.apply[A])
+  implicit val personFormat = jsonFormat2(Person)
+}
 
 object Db {
   def getPerson(name: String): Future[Option[Person]] = {
@@ -20,13 +28,11 @@ object Db {
     }
   }
 }
-
-object WebServer {
+object WebServer extends JsonSupport {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("web-server-system")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
-    implicit val personFormat = jsonFormat2(Person)
 
     val route =
       get {
@@ -34,13 +40,11 @@ object WebServer {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello</h1>"))
         } ~
         pathPrefix("person" / ".*".r) { name: String =>
-          val maybePerson = Db.getPerson(name)
-
-          onSuccess(maybePerson) {
-            case Some(person) => complete(person)
-            case None => complete(StatusCodes.NotFound)
+          rejectEmptyResponse {
+            complete {
+              Db.getPerson(name).map(x => x.map(x => Envelope(x)))
+            }
           }
-          //complete(Person("kot", 41))
         }
       }
 
